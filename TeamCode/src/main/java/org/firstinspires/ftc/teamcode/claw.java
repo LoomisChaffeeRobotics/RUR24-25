43db104f930fed4a4f26373377b544e7b7b1de91
Millen-LC <millen_berglind@loomis.org>
2024-10-08 16:27:56 -0400

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/claw.java
@@ -5,28 +5,24 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
-
-public class claw_setup extends OpMode {
-
+public class claw extends OpMode {
 
     Servo Claw;
-    float speedfactor = 0.001F;
-    float Clawclose = 0.7412F;
-    float Clawopen = 0.7367F;
+    float speedfactor = 0.002F;
+    float Clawclose = 0.4600F;
+    float Clawopen = 0.7500F;
 
 
     @Override
     public void init() {
+
         Claw = hardwareMap.get(Servo.class, "Claw");
-
-
         Claw.setPosition(Clawopen);
     }
 
 
     @Override
     public void loop() {
-
         if (gamepad1.back) {
             Claw.setPosition(Clawopen);
 
@@ -39,14 +35,17 @@ public class claw_setup extends OpMode {
         if (gamepad1.dpad_left && !gamepad1.left_bumper) {
             Claw.setPosition(Clawopen);
             telemetry.addData("Claw Pos:", (Claw.getPosition()));
-
-
-            if (gamepad1.dpad_right && gamepad1.left_bumper) {
-                Claw.setPosition(Claw.getPosition() - speedfactor);
-                telemetry.addData("Claw Pos:", (Claw.getPosition()));
-            }
-
         }
 
+
+        if (gamepad1.dpad_right && gamepad1.left_bumper) {
+            Claw.setPosition(Claw.getPosition() - speedfactor);
+            telemetry.addData("Claw Pos:", (Claw.getPosition()));
+        }
+        if (gamepad1.dpad_left && gamepad1.left_bumper) {
+            Claw.setPosition(Claw.getPosition() + speedfactor);
+            telemetry.addData("Claw Pos:", (Claw.getPosition()));
+        }
     }
+
 }
