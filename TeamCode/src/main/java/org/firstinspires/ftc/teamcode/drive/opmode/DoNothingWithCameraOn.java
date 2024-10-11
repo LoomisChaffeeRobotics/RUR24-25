be1462e31a6932c44306e8424db0eecd39af3382
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2024-10-11 13:51:07 -0400

    Camera thing ok
    its ok now. maybe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DoNothingWithCameraOn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DoNothingWithCameraOn.java
@@ -3,12 +3,23 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
 
 @TeleOp
 public class DoNothingWithCameraOn extends OpMode {
+
+    AprilTagProcessor myAprilTagProcessor;
+    VisionPortal myVisionPortal;
+
     @Override
     public void init() {
-
+// Create the AprilTag processor and assign it to a variable.
+        myAprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        // Enable or disable the AprilTag processor.
+        myVisionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), myAprilTagProcessor);
     }
 
     @Override
