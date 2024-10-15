00be1800b04b6748273173c2d175c50f9d286e53
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2024-10-15 16:12:49 -0400

    something that might work i don;tnknow hecause i haven t atesegrd yhye

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DoNothingWithCameraOn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DoNothingWithCameraOn.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -18,13 +19,14 @@ public class DoNothingWithCameraOn extends OpMode {
     IMU imu;
     AprilTagProcessor myAprilTagProcessor;
     VisionPortal myVisionPortal;
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
     @Override
     public void init() {
+
         imu = hardwareMap.get(IMU.class, "imu");
 // Create the AprilTag processor and assign it to a variable.
         myAprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
-        myAprilTagDetection = hardwareMap.get(AprilTagDetection.class, "myAprilTagDetection");
         // Enable or disable the AprilTag processor.
         myVisionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), myAprilTagProcessor);
 
@@ -33,7 +35,7 @@ public class DoNothingWithCameraOn extends OpMode {
     @Override
     public void loop() {
         aprilTagTelemetry();
-
+        telemetry.addData("pose",drive.getPoseEstimate());
     }
     private void aprilTagTelemetry() {
         List<AprilTagDetection> currentDetections = myAprilTagProcessor.getDetections();
